674544233e57ea84e5504fdd386fe7b12fd933b0
Utugoed <skorynin@appvelox.ru>
2022-04-04 07:56:14 -0400

    Webhook instead polling

--- app/config.py
+++ app/config.py
@@ -6,3 +6,12 @@ dotenv.load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_DB = "IB12"
 MONGO_URL = os.getenv("MONGO_URL")
+
+# webhook settings
+WEBHOOK_HOST = 'https://KuricynIsAGod.pythonanywhere.com'
+WEBHOOK_PATH = '/bot/{BOT_TOKEN}'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = 3001
--- bot.py
+++ bot.py
@@ -1,5 +1,25 @@
-from aiogram.utils import executor
+from aiogram.utils.executor import start_webhook
 
+from app import config
 from app.handlers import dp
+from app.loader import bot
 
-executor.start_polling(dp, skip_updates=True)
+
+async def on_startup():
+    bot.set_webhook(config.WEBHOOK_URL)
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+if __name__ == "__main__":
+    start_webhook(
+        dispatcher = dp,
+        webhook_path=config.WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=config.WEBAPP_HOST,
+        port=config.WEBAPP_PORT,
+    )
